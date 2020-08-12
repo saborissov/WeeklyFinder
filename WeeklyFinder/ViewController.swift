9539aee6d492f7dd2cb0d4215fc7882659c54c18
17586317 <Borisov.S.Alek@sberbank.ru>
2020-08-13 00:08:00 +0300

    Initial Commit

---
+++ WeeklyFinder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeeklyFinder
+//
+//  Created by 17586317 on 13.08.2020.
+//  Copyright © 2020 Борисов Сергей Александрович. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
